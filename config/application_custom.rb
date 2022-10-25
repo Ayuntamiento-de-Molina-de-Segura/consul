0535c5d3f8029f596b6a88a0465c8050a6850f86
Diego Calvo <diego.calvo@enreda.coop>
2022-10-25 16:31:01 +0200

    Spanish by default fixed

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,10 +1,6 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :es
-    available_locales = [
-      "es",
-      "en"
-      ]
-    config.i18n.available_locales = available_locales
+    config.i18n.available_locales = [:es, :en]
   end
 end
